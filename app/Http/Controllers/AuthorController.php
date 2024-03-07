89c669010d45ba2c34eea353def784a1c4c45b11
Ninhisme <91234552+Ninhisme@users.noreply.github.com>
2024-03-07 11:16:45 +0700

    ver_1

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    public function AuthorDashboard(){
+        return view('author.author_dashboard');
+    }
+}
